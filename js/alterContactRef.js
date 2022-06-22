1998304fe9793b8b765fef7e78d3901cf19004a2
Allen Shaw <allen@JoineryHQ.com>
2022-06-22 10:56:48 -0500

    Remove debug code in order to use actual live custom field id.

--- js/alterContactRef.js
+++ js/alterContactRef.js
@@ -1,10 +1,11 @@
 (function($){
   $(window).load(function(){
     if (CRM.vars.fpptaqbhelper.contactRefCustomFieldId != undefined) {
-      var opts = $('#custom_12').data('select2').opts;
+      var selector = '#custom_' + CRM.vars.fpptaqbhelper.contactRefCustomFieldId;
+      var opts = $(selector).data('select2').opts;
       console.log('window load opts', opts);
       opts.ajax.data=function(term){return {isFpptaqbhelperContactRef: '1', term: term}};
-      $('#custom_12').crmSelect2(opts);
+      $(selector).crmSelect2(opts);
     };
   });
 })(cj || CRM.$ || jQuery);
