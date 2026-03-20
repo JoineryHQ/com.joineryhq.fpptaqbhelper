14c6be1322cd773aa2eae92c656098ae7d439a8a
Allen Shaw <allen@JoineryHQ.com>
2026-03-20 18:21:20 -0500

    civix upgrade, format: 25.10.2

--- fpptaqbhelper.php
+++ fpptaqbhelper.php
@@ -24,59 +24,12 @@ function fpptaqbhelper_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function fpptaqbhelper_civicrm_postInstall() {
-  _fpptaqbhelper_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function fpptaqbhelper_civicrm_uninstall() {
-  _fpptaqbhelper_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function fpptaqbhelper_civicrm_enable() {
   _fpptaqbhelper_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function fpptaqbhelper_civicrm_disable() {
-  _fpptaqbhelper_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function fpptaqbhelper_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _fpptaqbhelper_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function fpptaqbhelper_civicrm_entityTypes(&$entityTypes) {
-  _fpptaqbhelper_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
